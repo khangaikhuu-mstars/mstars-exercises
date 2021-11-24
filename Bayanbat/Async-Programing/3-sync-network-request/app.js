c378f82350f78ca52ce887eb0e5ba41fcdabb5b4
lab_1_10 <lab_1_10@lab-1-10s-iMac.local>
2021-11-24 14:30:08 +0800

    add sync4 file

--- Bayanbat/Async-Programing/3-sync-network-request/app.js
+++ Bayanbat/Async-Programing/3-sync-network-request/app.js
@@ -1,14 +1,16 @@
 function getJSON(url) {
-  const xhr = new XMLHttpRequest();
+  var xhr = new XMLHttpRequest();
   xhr.open('GET', url, false);
   xhr.onreadystatechange = function () {
     if(xhr.readyState === 4 && xhr.status === 200) {
       let data = JSON.parse(xhr.responseText);
       data.map(p => console.log(p));
+      document.querySelector("p").innerHTML = data
     }
   };
   xhr.send(null);
 }
 
 getJSON('https://jsonplaceholder.typicode.com/photos');
-alert('Welcome to this page!');
+alert('Welcome to this page!'); 
+// document.querySelector("p").innerHTML = data
