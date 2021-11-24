77e8fd98f8b515ff50727f390f5e85d9e309a106
Mstars_lab1_20 <mstarsacademy@MStarss-iMac-1.local>
2021-11-24 10:58:03 +0800

    adding app.js

--- tsedevee/ajax-basics/ajax_basics_exercise/ex-01/Exercise-01/app.js
+++ tsedevee/ajax-basics/ajax_basics_exercise/ex-01/Exercise-01/app.js
@@ -4,7 +4,7 @@ xhr.onreadystatechange = function (){
     console.log(xhr.status);
     console.log(xhr.responseText);
     document.getElementById("index").innerHTML = xhr.responseText;
-
+}
 xhr.open('GET', 'sidebar.html');
 
 xhr.send();
