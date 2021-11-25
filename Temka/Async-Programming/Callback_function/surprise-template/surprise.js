5911d87c1b2fdd08b507b15027095ec4e4ba5328
Mstars_Lab1_13 <mstars_lab1_13@Mstars-Lab1-13s-iMac.local>
2021-11-25 10:24:59 +0800

    edited surprise

--- Temka/Async-Programming/Callback_function/surprise-template/surprise.js
+++ Temka/Async-Programming/Callback_function/surprise-template/surprise.js
@@ -5,21 +5,22 @@
 
 const surpriseSection = document.getElementById('surprise');
 
-// function showSurprise() {
-//     surpriseSection.textContent = '🎉 Surprise! 🎉';
-// }
-
-
-// let showSurprise = function(){
-//     surpriseSection.textContent = '🎉 Surprise! 🎉';
-// }
-
-let showSurprise =()=> {
+function showSurprise() {
     surpriseSection.textContent = '🎉 Surprise! 🎉';
 }
 
-
+//setTimeout(showSurprise, 2000);
 
 var timer  = Math.random() * 4000
 
-setTimeout(showSurprise,timer)
+// setTimeout(showSurprise,timer)
+
+
+// setTimeout(function(){
+//     surpriseSection.textContent = '🎉 Surprise! 🎉';
+// } , timer)
+
+
+setTimeout(()=>{
+    surpriseSection.textContent = '🎉 Surprise! 🎉';
+},timer)
