a5a0678362bd0eed5fa24a33702f15ddcf1c8790
Mstars_Lab1_11 <mstars_lab1_11@Mstars-Lab1-11s-iMac.local>
2021-11-25 10:42:13 +0800

    Adding callback argument task

--- angarag/Async - programming/callback-argument/app.js
+++ angarag/Async - programming/callback-argument/app.js
@@ -10,7 +10,17 @@ function makeRed(element) {
 function makeBlue(element) {
     element.style.backgroundColor = "blue";
 }
+function makeGreen(element){
+    element.style.backgroundColor = "green"
+}
+
 
 function addStyleToElement(element, callback) {
     callback(element);
 }
+
+
+
+addStyleToElement(div1, makeRed);
+addStyleToElement(div2, makeBlue);
+addStyleToElement(div3, makeGreen);
