65ae440cba30c218e2cba398f468df6b63a97d84
Mstars_Lab1_17 <mstarsacademy@Mstarss-iMac.local>
2021-11-30 12:52:17 +0800

    [ADD] Object-Oriented-JS folder and files

---
+++ Amarsanaa/Object-Oriented-Javascript/object-literal.js
@@ -0,0 +1,23 @@
+const dog = {
+  name: "Bankhar",
+  breed: "Mongolia",
+  voice: function () {
+    console.log("woof");
+  },
+};
+const cat = {
+  name: "Lucy",
+  breed: "British",
+  voice: function () {
+    console.log("meow");
+  },
+};
+const cow = {
+  name: "Alagaa",
+  breed: "Khainag",
+  voice: function () {
+    console.log("umbuu");
+  },
+};
+
+dog.voice();
