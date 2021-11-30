11caef1c9a2a67311f561259fedd58dc86b65d9c
Mstars_Lab1_12 <mstars_lab1_12@Mstars-Lab1-12s-iMac.local>
2021-11-30 12:26:58 +0800

    addid Fetch

---
+++ tuvshinbat/Async-programing/T5_10_Callback/promise.js
@@ -0,0 +1,50 @@
+// Доорх Promise функц өгөгдсөн бөгөөд
+// үүнийг ашиглан callback_hell.js дээрх давхардсан setTimeout
+// функцыг хувирган бичнэ үү.
+//
+function PromisifyingTimeout(fn, time) {
+  return new Promise(function (resolve, reject) {
+    setTimeout(function () {
+      try {
+        resolve(fn())
+      } catch (e) {
+        reject(e)
+      }
+    }, time)
+  })
+}
+
+function countThree() {
+  console.log('3...');
+}
+
+function countTwo() {
+  console.log('2...');
+}
+
+function countOne() {
+  console.log('1...');
+
+}
+
+function done() {
+  console.log('done...');
+
+}
+// гэх мэтээр үргэлжлүүлэн бусад хэрэгтэй функцүүдийг бичнэ үү
+
+// Даалгаврыг эндээс эхлэн хийнэ үү
+PromisifyingTimeout(countThree, 1000)
+  .then(function () {
+      return PromisifyingTimeout(countTwo, 1000)
+    })
+    .then(function () {
+      return PromisifyingTimeout(countOne, 1000)
+    })
+    
+    .then(function () {
+      return PromisifyingTimeout(done, 1000)
+    })
+
+    
+  
--- tuvshinbat/ajax-basics/Ajax_Basics_Exercise/Ex-01/Exercise-01/app.js
+++ tuvshinbat/ajax-basics/Ajax_Basics_Exercise/Ex-01/Exercise-01/app.js
@@ -1,14 +1,20 @@
 // bodlog 1
 var xhr = new XMLHttpRequest;
+var xhrResult = '';
 xhr.onload = function () {
     if (xhr.status === 200) {
         console.log("the Request is ok");
         document.getElementById("ajax").innerHTML = "the Request is ok";
+        const response = xhr.responseText;
+        xhrResult = response;
 
+
+    } else{
+        console.log("responses wrong !!!")
     }
 
 }
 xhr.open("GET", "sidebar.html");
 xhr.send();
 
-//bodlog 2
+
