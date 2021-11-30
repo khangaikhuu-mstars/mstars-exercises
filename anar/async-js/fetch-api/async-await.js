6d8dfbf4c479fc9e3c5804c9016597402815e841
Mstars_lab1_16 <mstarsacademy@MStarss-Anar.local>
2021-11-30 12:15:37 +0800

    copy files

--- anar/async-js/fetch-api/async-await.js
+++ anar/async-js/fetch-api/async-await.js
@@ -21,10 +21,15 @@ async function fetchJson() {
     return data.json();
 }
 
-fetchJson().then(data => ajax.innerHTML = data.map(i => {
-    console.log(i);
-}))
-
+fetchJson()
+    .then(data =>{
+        console.log('Async function starts here');
+        ajax.innerHTML = data.map(i => {
+            console.log(i);
+        })
+    })
+    .catch(() => console.log("Error occured !"))
+    .finally(() => console.log("Async function ends"));
 // 2. хэрвээ fetch функц рүү хөрвүүлсэн бол async/await ашиглан шинээр async функц шинээр тодорхойлоод fetch ашиглан pets.json аваад тэр утгаа буцаадаг болгоно уу
 
 
