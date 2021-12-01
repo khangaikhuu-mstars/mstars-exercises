3bd128b8784e25b3feb979073476e5565a69cc4b
Mstars_Lab1_12 <mstars_lab1_12@Mstars-Lab1-12s-iMac.local>
2021-12-01 12:59:55 +0800

    temka

--- tuvshinbat/object/object_literal.js
+++ tuvshinbat/object/object_literal.js
@@ -3,7 +3,7 @@ const pets = ['cat', 'dog', 'temee', 'ymaa', 'uher', 'honi', 'mori', 'tuulai', '
 const cat = {
     name: 'miav miav',
     nas: '1',
-    voice : function () {
+    voice: function () {
         console.log('how how')
     }
 }
@@ -12,63 +12,63 @@ const cat = {
 const dog = {
     name: 'banhar',
     nas: '1',
-    voice : function () {
+    voice: function () {
         console.log('how how')
     }
 }
 const camel = {
     name: 'usa',
     age: '2',
-    voice : function () {
+    voice: function () {
         console.log('jkhjkhjhfgh')
     }
 }
 const ymaa = {
     name: 'Suhee',
     age: '3',
-    voice : function () {
+    voice: function () {
         console.log('mai mai')
     }
 }
 const uher = {
     name: 'buh',
     age: '4',
-    voice : function () {
+    voice: function () {
         console.log('umbuu umbuu')
     }
 }
 const honi = {
     name: 'hurga',
     age: '5',
-    voice : function () {
+    voice: function () {
         console.log('mai mai')
     }
 }
 const mori = {
     name: 'shudlen',
     age: '6',
-    voice : function () {
+    voice: function () {
         console.log('iiihohohoho')
     }
 }
 const tuulai = {
     name: 'misheel',
     age: '7',
-    voice : function () {
+    voice: function () {
         console.log('fgfgfgf')
     }
 }
 const usanGaihai = {
     name: 'angel',
     age: '8',
-    voice : function () {
+    voice: function () {
         console.log('sssss')
     }
 }
 const ystMelhii = {
     name: 'melhii',
     age: '9',
-    voice : function () {
+    voice: function () {
         console.log('ilili')
     }
 }
