42e0e6ddfa861b60b84bbee3055d5a815460d41a
Mstars_lab1_05 <mstars_lab1_05@Mstars-lab1-05s-iMac.local>
2021-12-02 10:49:13 +0800

    adding to Ship.js file

--- Tserendorj/object-Oriented-Js/Ship.js
+++ Tserendorj/object-Oriented-Js/Ship.js
@@ -5,46 +5,53 @@ class Ship {
         this.width = weight;
         this.type = type;
     }
-    get swimming(){
+    get swimming() {
         console.log('swimming');
     }
-    get activity(){
+    get activity() {
         const today = new Date();
         return today
     }
-    get info(){
+    get info() {
         return `This is ${this.type} ship. i have height: ${this.heigth}, weight: ${this.weight}, widht: ${this.width}`
     }
-    set shipType(t){
+    set shipType(t) {
         this.type = t;
     }
+    set changeOwner(owner) {
+        this._owner = owner;
+    }
+    get owner() {
+        return this._owner;
+    }
 };
-const fighterShip = new Ship(7,10,5,'fighter');
+const fighterShip = new Ship(7, 10, 5, 'fighter');
 fighterShip.shipType = "pirate"
 console.log(fighterShip);
-const battleShip = new Ship(7,10,5,'battle');
+const battleShip = new Ship(7, 10, 5, 'battle');
 console.log(battleShip);
-const fishingShip = new Ship(7,10,5,'fishing');
+const fishingShip = new Ship(7, 10, 5, 'fishing');
 console.log(fighterShip)
-const spiShip = new Ship(7,10,5,'spiTool');
+const spiShip = new Ship(7, 10, 5, 'spiTool');
 console.log(spiShip)
 fighterShip.swimming;
 console.log(fighterShip.activity)
 console.log(fighterShip.info)
 
-class Owner{
-    constructor(name, address, phone){
+class Owner {
+    constructor(name, address, phone) {
         this.name = name;
         this.address = address;
         this.phone = phone;
     }
-    set changeOwner(owner){
-        this._owner = owner;
+    name() {
+        return this.name
     }
 };
-const employe = new Owner('Tserendorj','UB 26r Khoroo OlympVillage','99489843');
+const employe = new Owner('Tserendorj', 'UB 26r Khoroo OlympVillage', '99489843');
 const company = new Owner('Usukh_Od Table Tennis Club', 'Chuhag Khothon', '85026802');
 const company2 = new Owner('MCS', 'Ulaanbaatar', '+1234567890');
 
-fighterShip.changeOwner = 'mcs';
+fighterShip.changeOwner = company2;
 console.log(fighterShip)
+console.log(fighterShip.owner.name)
