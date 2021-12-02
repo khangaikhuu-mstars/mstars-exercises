7237908b821e4fc6b01ede6b3a3b7bb224ee9324
lab_1_10 <lab_1_10@lab-1-10s-iMac.local>
2021-12-02 10:38:24 +0800

    add file ship js

--- Bayanbat/object_oriented_js/Ship.js
+++ Bayanbat/object_oriented_js/Ship.js
@@ -16,6 +16,12 @@ class Ship {
     get info() {
         return `This is ${this.type} ship. bla3 ${this.height}.bla3 ${this.width}.bla3 ${this.weight}.`
     }
+    set hangover(i) {
+        this.type = i;
+    }
+    set changeOwwner(Owner) {
+        this_owner = owner;
+    }
 };
 const fighterShip = new Ship(7, 10, 5, "fighter");
 const containerShip = new Ship(7, 10, 5, "container");
@@ -28,8 +34,19 @@ console.log(navalShip);
 console.log(passengerShip);
 console.log(tankerShip);
 fighterShip.type = "hangover";
+fighterShip.hangover = 'pirate';
 console.log(fighterShip);
 console.log(fighterShip.swimming)
 console.log(fighterShip.activity)
 console.log(navalShip.activity)
 console.log(navalShip.info)
+class Owner {
+    constructor(name, address, phone) {
+        this.name = name,
+            this.address = address,
+            this.phone = phone
+    }
+}
+const bayanbat = new Owner("Bayanbat", "Sukhbaatar duureg bla3", "8675565");
+fighterShip.changeOwner = bayanbat;
+console.log(fighterShip);
