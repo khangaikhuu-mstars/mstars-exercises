59c9ff880ddb9aabdbddaae6568e2bc555aff5ab
Mstars_lab1_16 <mstarsacademy@MStarss-Anar.local>
2021-12-02 10:30:31 +0800

    git fix

--- anar/async-js/object-orientedljs/oo-js.js
+++ anar/async-js/object-orientedljs/oo-js.js
@@ -1,43 +1,58 @@
 
-// [
-    class Ship {
-        constructor(h, w, tp, wt,) {
-            this.height = h;
-            this.width = w;
-            this.type = tp;
-            this.weight = wt;
-        }
-        get status() {
-            console.log('Status:');
-            console.log('active');
-            console.log('roaming');
-            console.log('swimming');
-            console.log('--------');
-        };
-        get activity() {
-            const today = new Date;
-            return today;
-        }
-        get info(){
-            return console.log(`
-                    This is a ${this.type} ship.
-                    Height: ${this.height}
-                    Weight: ${this.weight}
-                    Width: ${this.width}
-                    `)
-        }
-        set shipType(type) {
-            this.type = type;
-        }
+class Ship {
+    constructor(h, w, tp, wt,) {
+        this.height = h;
+        this.width = w;
+        this.type = tp;
+        this.weight = wt;
     }
-// ]
+    get status() {
+        console.log('Status:');
+        console.log('active');
+        console.log('roaming');
+        console.log('swimming');
+        console.log('--------');
+    };
+    get activity() {
+        const today = new Date;
+        return today;
+    }
+    get info(){
+        return console.log(`
+                This is a ${this.type} ship.
+                Height: ${this.height}
+                Weight: ${this.weight}
+                Width: ${this.width}
+                `)
+    }
+    set shipType(type) {
+        this.type = type;
+    }
+}
 
-const fighterShip = new Ship(20, 40, 'fighter', 10000);
-console.log(fighterShip);
-fighterShip.shipType = 'pirate';
-fighterShip.info;
-fighterShip.status;
-console.log(fighterShip.activity);
+class Owner{
+    constructor(name, address, phone) {
+        this.name = name;
+        this.address = address;
+        this.phone = phone;
+    }
+}   
+
+
+const person = new Owner('Anar', 'Ulaanbaatar', 88887777);
+console.log(person);
+const company = new Owner('MCS', 'Ulaanbaatar', 77778888);
+console.log(company);
+
+
+
+
+// const fighterShip = new Ship(20, 40, 'fighter', 10000);
+// console.log(fighterShip);
+// fighterShip.shipType = 'pirate';
+// fighterShip.info;
+// fighterShip.status;
+// console.log(fighterShip.activity);
 
 
 
