99ae15b84c45409f57ca912a13347a75be388f0f
tuvshinbat t <tuvshinbat@gmail.com>
2021-12-04 23:32:14 +0800

    home worked added tal

---
+++ tuvshinbat/Homework_Dec_03/User.js
@@ -0,0 +1,11 @@
+class Daalgavar {
+    constructor(email, userName, birthday){
+        this.email = email;
+        this.userName = userName;
+        this.birthday = birthday;
+    }
+}
+
+let user1 = new Daalgavar('Javascript@mstars.mn','JSUser1','01/08/1991')
+   
+console.log(user1)
