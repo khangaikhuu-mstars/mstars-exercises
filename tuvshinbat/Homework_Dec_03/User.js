d8311cc23b1613def41adfad00788ca86f6c04c0
tuvshinbat t <tuvshinbat@gmail.com>
2021-12-05 11:37:48 +0800

    Homeword_dec_03 add

--- tuvshinbat/Homework_Dec_03/User.js
+++ tuvshinbat/Homework_Dec_03/User.js
@@ -4,8 +4,12 @@ class Daalgavar {
         this.userName = userName;
         this.birthday = birthday;
     }
+    changeUsername(){
+      return   ` ene bol shin hereglegchiin ner ${this.userName}`
+    }
 }
 
 let user1 = new Daalgavar('Javascript@mstars.mn','JSUser1','01/08/1991')
-   
+let user2 = new Daalgavar('JSmstars','Tuvshinbat','1990.04.19',)   
 console.log(user1)
+console.log(user2.changeUsername())
