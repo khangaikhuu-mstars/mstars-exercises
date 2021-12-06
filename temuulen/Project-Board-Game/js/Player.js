616c46575f62241df9a16f9468775b559b30d453
Mstars_lab1_07 <mstars_lab1_07@Mstars-lab1-07s-iMac.local>
2021-12-06 14:04:08 +0800

    file

--- temuulen/Project-Board-Game/js/Player.js
+++ temuulen/Project-Board-Game/js/Player.js
@@ -4,19 +4,19 @@ class Player {
         this.id = id;
         this.color = color;
         this.active = active;
-        this.tokens = this.createTokens(21);
+        this.tokens = this.createToken(21);
     }
 
-    //create player token
-    createTokens(number) {
+    // create players token
+    createToken(number) {
         const tokens = [];
-        for(let i = 0; i < number; i++) {
-            let token = new Token(i, this);
+        for (let i = 0; i < number; i++) {
+            const token = new Token(i, this);
             tokens.push(token);
         }
         return tokens;
     }
-    
+
     get unusedTokens() {
         return this.tokens.filter(token => !token.dropped);
     }
@@ -24,5 +24,7 @@ class Player {
     get activeToken() {
         return this.unusedTokens[0];
     }
-
+    checkTokens() {
+        return this.tokens.length !== 0;
+    }
 }
