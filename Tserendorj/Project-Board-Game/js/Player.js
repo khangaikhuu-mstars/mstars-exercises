903e991de6a3bdfba5e461d58985d7d7204f6af6
Mstars_lab1_05 <mstars_lab1_05@Mstars-lab1-05s-iMac.local>
2021-12-16 10:07:19 +0800

    adding to protoTypes

--- Tserendorj/Project-Board-Game/js/Player.js
+++ Tserendorj/Project-Board-Game/js/Player.js
@@ -1,11 +1,11 @@
 class Player {
-    constructor(name, id,color, active = false) {
-        
+    constructor(name, id, color, active = false) {
+
         this.name = name;
         this.id = id;
         this.color = color;
         this.active = active;
-        this.token = this.createTokens(21)
+        this.tokens = this.createTokens(21)
     }
 
     //create player token
@@ -13,18 +13,15 @@ class Player {
     createTokens(number) {
         const tokens = [];
         for (let i = 0; i < number; i++) {
-             const token = new Token(i, this);
-             tokens.push(token);
+            const token = new Token(i, this);
+            tokens.push(token);
         }
         return tokens;
     }
-     
-    get unusedTokens(){
-        return this.tokens.filter(token =>!token.dropped);
+    get unusedTokens() {
+        return this.tokens.filter(token => !token.dropped);
     }
-    get activeToken(){
-       return this.unusedTokens[0];
+    get activeToken() {
+        return this.unusedTokens[0];
     }
-
-};
-
+}
