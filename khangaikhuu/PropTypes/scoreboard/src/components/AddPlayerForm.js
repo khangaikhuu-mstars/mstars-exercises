6ee0dd4d7d64c5444294cbc9429cf6c8ebb99b02
khangaikhuu uvgunkhuu <khangaikhuu@mstars.mn>
2021-12-20 13:02:09 +0800

    [ADD] prop types

--- khangaikhuu/PropTypes/scoreboard/src/components/AddPlayerForm.js
+++ khangaikhuu/PropTypes/scoreboard/src/components/AddPlayerForm.js
@@ -1,30 +1,37 @@
 import React, { Component } from 'react';
+import { Consumer } from './Context';
 
 class AddPlayerForm extends Component {
 
   playerInput = React.createRef();
 
-  handleSubmit = (e) => {
-    e.preventDefault();
-    this.props.addPlayer(this.playerInput.current.value);
-    e.currentTarget.reset();
-  }
 
   render() {
     return (
-      <form onSubmit={this.handleSubmit}>
-        <input 
-          type="text"
-          ref={this.playerInput}
-          placeholder="Enter a player's name"
-        />
-        
-        <input 
-          type="submit"
-          value="Add Player"
-        />
-      </form>
-    );
+      <Consumer>
+        {context => {
+           context.actions.handleSubmit = (e) => {
+            e.preventDefault();
+            context.actions.addPlayer(this.playerInput.current.value);
+            e.currentTarget.reset();
+          }
+          return (
+            <form onSubmit={context.actions.handleSubmit}>
+              <input 
+                type="text"
+                ref={this.playerInput}
+                placeholder="Enter a player's name"
+              />
+              
+              <input 
+                type="submit"
+                value="Add Player"
+              />
+            </form>
+          )
+        }}
+      </Consumer>
+    )
   }
 }
 
