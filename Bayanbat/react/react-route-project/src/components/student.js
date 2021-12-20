910d7649db097407a1861e3323c5e4d87b955cc2
lab_1_10 <lab_1_10@lab-1-10s-iMac.local>
2021-12-20 12:59:36 +0800

    add gif-search & react-route-project

---
+++ Bayanbat/react/react-route-project/src/components/student.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Student = () => {
+    return (
+        <div className="main-content ">
+            <h1>My name</h1>
+            <p>KhangaiKhuu</p>
+        </div>
+    )
+
+}
+
+export default Student;
