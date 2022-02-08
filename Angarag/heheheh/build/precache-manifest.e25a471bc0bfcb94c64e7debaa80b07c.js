e514e21f2d264b9f462951f6711091ddadc5de76
Mstars_Lab1_11 <mstars_lab1_11@Mstars-Lab1-11s-iMac.local>
2022-02-08 13:08:56 +0800

    HHEHEH

---
+++ Angarag/heheheh/build/precache-manifest.e25a471bc0bfcb94c64e7debaa80b07c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "67464695aac8ac183c9b",
+    "url": "/static/css/main.b7892c47.chunk.css"
+  },
+  {
+    "revision": "67464695aac8ac183c9b",
+    "url": "/static/js/main.67464695.chunk.js"
+  },
+  {
+    "revision": "f1114e3871cf27b0485b",
+    "url": "/static/js/1.f1114e38.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "869957058e6e51627e6658e9c08e7ed9",
+    "url": "/index.html"
+  }
+];
