3f10d004f179a8edbce0c08c27ed7d64f7ed751b
lab_1_0 <lab_1_02@lab-1-02s-iMac.local>
2022-04-15 12:30:47 +0800

    file

---
+++ munkhzul/PropTypes/scoreboard/src/components/Stats.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const Stats = (props) => {
+ 
+  const totalPlayers = props.players.length;
+  const totalPoints = props.players.reduce( (total, player) => {
+    return total + player.score;
+  }, 0);
+
+  return (
+    <table className="stats">
+      <tbody>
+        <tr>
+          <td>Players:</td>
+          <td>{ totalPlayers }</td>
+        </tr>
+        <tr>
+          <td>Total Points:</td>
+          <td>{ totalPoints }</td>
+        </tr>
+      </tbody>
+    </table>
+  );
+}
+
+Stats.propTypes = {
+  props: PropTypes.shape({
+    players: PropTypes.arrayOf(PropTypes.objectOf(
+      {
+        score: PropTypes.number.isRequired
+      }
+    ))
+  })
+}
+
+export default Stats;
